4934ff63de7e749f4405d30f3402efc68a36245d
mayusaito-div <mayu0711mayu0711ry@gmail.com>
2026-08-20 15:00:45 +0900

    contactform

---
+++ contactform/insert.php
@@ -0,0 +1,29 @@
+<?php
+mb_internal_encoding("utf8");
+$pdo=new PDO("mysql:host=localhost;dbname=lesson01;charset=utf8mb4","root","");
+$sql="INSERT INTO contactform(name,mail,age,comment)VALUES(?,?,?,?)";
+$stml=$pdo->prepare($sql);
+
+$stml->bindValue(1,$_POST['userName']);
+$stml->bindValue(2,$_POST['mail']);
+$stml->bindValue(3,$_POST['age']);
+$stml->bindValue(4,$_POST['comment']);
+$stml->execute();
+?>
+
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>お問い合わせフォーム</title>
+    <link rel="stylesheet"href="style.css">
+</head>
+
+<body>
+    <h1 class="title">お問い合わせが完了しました</h1>
+    <div class="content">
+        <div>お問い合わせありがとうございました。</div>
+        <div>3営業日以内に担当者よりご連絡差し上げます。</div>
+</div>
+</body>
+</html>
